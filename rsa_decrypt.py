195c3cae5da713a1797c6b9f0609005c42742f08
C-SungminLee <91634957+C-SungminLee@users.noreply.github.com>
2022-01-26 17:27:39 -0800

    Add files via upload

--- rsa_decrypt.py
+++ rsa_decrypt.py
@@ -1,10 +1,25 @@
 #   a212_rsa_decrypt.py
 import rsa as rsa
 
-key = int(input("Enter the Decryption Key: " ))
-mod_value = int(input("Enter the Modulus: " ))
-encrypted_msg = input("What message would you like to decrypt (No brackets): ")
+key = (input("Enter the Decryption Key: " ))
+while key.isdigit() != True:
+  print("Must insert Number.")
+  key = (input("Enter the Decryption Key: " ))
+
+else:
+  mod_value = (input("Enter the Modulus: " ))
+
+
+while mod_value.isdigit() != True:
+  print("Must insert Number.")
+  mod_value = (input("Enter the Modulus: " ))
+  
+
+else:
+  encrypted_msg = input("What message would you like to decrypt (No brackets): ")
+
+
 
 #break apart the list that is cut/copied over on ", "
 msg = encrypted_msg.split(", ")
-print (rsa.decrypt(key,mod_value , msg))
+print (rsa.decrypt(int(key),int(mod_value) , msg))
--- validate_user_input.py
+++ validate_user_input.py
@@ -1,4 +1,11 @@
 #   a212_rsa_encrypt.py
+
+# This program validates input from a user
+'''
+isalpha. This method returns True if all characters in the string are alphabetic (a-z or A-Z) 
+and there is at least one character in the string. Otherwise, it returns False.
+
+'''
 import rsa as rsa
 
 key = (input("Enter the Encryption Key: " ))
